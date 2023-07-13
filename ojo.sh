3298c47fd382fe0c6c3d450c113151578c19f128
molla202 <91562185+molla202@users.noreply.github.com>
2023-07-13 03:52:06 +0300

    Update ojo.sh

--- ojo.sh
+++ ojo.sh
@@ -3,20 +3,6 @@ clear
 curl -sSL https://raw.githubusercontent.com/molla202/Scripts/main/molla202.sh | bash
 install_binary() {
 exec > /dev/null 2>&1
-sudo apt update && sudo apt upgrade -y
-sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
-cd $HOME
-! [ -x "$(command -v go)" ] && {
-VER="1.19.3"
-wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
-rm "go$VER.linux-amd64.tar.gz"
-[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
-echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
-source $HOME/.bash_profile
-}
-[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
 cd $HOME
 rm -rf ojo
 git clone https://github.com/ojo-network/ojo.git
